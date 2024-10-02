0af7836e853a504ea4a6600c7148824edc4eb067
jonathanchr1 <165112945+jonathanchr1@users.noreply.github.com>
2024-10-02 21:32:14 +0700

    Add files via upload

---
+++ storage/framework/views/552d3a7dc94ef074763311cf264b1d9b.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EduReach</title>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="<?php echo e(asset('bootstrap/css/bootstrap.min.css')); ?>">
+</head>
+<body>
+    <div class="container-fluid text-center">
+        <!-- <div class="row">
+            <div class="col-2">
+                <img src="<?php echo e(asset('img/logo-edureach.jpg')); ?>" alt="EduReach" width="86.75" height="41.5">
+            </div>
+            <div class="col-10"></div>
+        </div> -->
+        <div class="row" style="background-color: wheat">
+            <div class="col-12">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: wheat">
+                    <div class="container-fluid">
+                        <img src="<?php echo e(asset('img/logo-edureach.jpg')); ?>" alt="EduReach" width="86.75" height="41.5" style="">
+                        <a class="navbar-brand" href="#"> </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNavAccount" style="padding-left: 1580px">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#" style="border: 1px solid">Log in</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#"> </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#" style="border: 1px solid">Register</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+        <div class="row" style="background-color: wheat">
+            <div class="col-12">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: wheat">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">EduReach</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">About Us</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Class</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Donate</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Dropdown link
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="#">Action</a></li>
+                                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div>
+        <h1 style="padding-left: 25px">Hello There</h1>
+        
+    </div>
+</body>
+</html><?php /**PATH F:\WebProg\CobaWebEduReach\laravel-project\resources\views/home.blade.php ENDPATH**/ ?>
